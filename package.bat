bde3c412c01f005d4b6c74e99767e09c7aa5a5bd
Splatbeef <legohaas2003@gmail.com>
2026-08-18 19:12:07 +0200

    Add icon to the package

--- package.bat
+++ package.bat
@@ -3,6 +3,7 @@ rmdir /s /q build
 pyinstaller --onedir ^
     --windowed ^
     --add-data "seeding_data;seeding_data" ^
+    --icon "icon.ico" ^
     --name LMUStrategyTool ^
     --noconfirm ^
     main.py 
